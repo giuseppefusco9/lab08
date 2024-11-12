8d0464ca7173fd072f152cbb504ad71a9abd7f15
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2024-11-12 21:14:36 +0100

    82 complete

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -0,0 +1,116 @@
+package it.unibo.deathnote.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImplementation implements DeathNote {
+
+    private Map<String, Death> deathNote;
+    private String lastNameWritten;
+
+    public DeathNoteImplementation(){
+        this.deathNote = new HashMap<>();
+        this.lastNameWritten = "";
+    }
+
+    @Override
+    public String getRule(int ruleNumber) {
+        if(ruleNumber < 1 || ruleNumber > RULES.size()){
+            throw new IllegalArgumentException("ruleNumber < 1 or > than the number of rules");
+        }
+        return RULES.get(ruleNumber - 1);
+    }
+
+    @Override
+    public void writeName(String name) {
+        if(name == null){
+            throw new NullPointerException("The string given is null");
+        }
+        this.lastNameWritten = name;
+        deathNote.put(name, new Death());
+    }
+
+    @Override
+    public boolean writeDeathCause(String cause) {
+        if(deathNote.isEmpty() || cause == null){
+            throw new IllegalStateException("the string given is null or Death Note doesn't contain any name");
+        }
+        return deathNote.get(lastNameWritten).setCause(cause);
+    }
+
+    @Override
+    public boolean writeDetails(String details) {
+        if(deathNote.isEmpty() || details == null){
+            throw new IllegalStateException("the string given is null or Death Note doesn't contain any name");
+        }
+        return deathNote.get(lastNameWritten).setDetails(details);
+    }
+
+    @Override
+    public String getDeathCause(String name) {
+        if(!deathNote.containsKey(name)){
+            throw new IllegalArgumentException("The name is not in the death note");
+        }
+        return deathNote.get(name).getCause();
+    }
+
+    @Override
+    public String getDeathDetails(String name) {
+        if(!deathNote.containsKey(name)){
+            throw new IllegalArgumentException("The name is not in the death note");
+        }
+        return deathNote.get(name).getDetails();
+    }
+
+    @Override
+    public boolean isNameWritten(String name) {
+        return deathNote.containsKey(name);
+    }
+
+    private class Death{
+        private static final String DEFAULT_CAUSE = "heart attack";
+        private static final int VALID_DETAILS_MS = 6040;
+        private static final int VALID_CAUSE_MS = 40;
+        private String cause;
+        private String details;
+        private long deathTime;
+
+
+        public Death(){
+            this.deathTime = System.currentTimeMillis();
+            this.cause = DEFAULT_CAUSE;
+            this.details = "";
+        }
+
+        public boolean setCause(String cause){
+            if(System.currentTimeMillis() < deathTime + VALID_CAUSE_MS){
+                this.cause = cause;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public boolean setDetails(String details){
+            if(System.currentTimeMillis() < deathTime + VALID_DETAILS_MS){
+                this.details = details;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        public String getCause(){
+            return this.cause;
+        }
+
+        public String getDetails(){
+            return this.details;
+        }
+    }
+
+}
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,90 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImplementation;
+
 class TestDeathNote {
+
+    private static final int MS_TO_SLEEP_6100 = 6100;
+    private static final int MS_TO_SLEEP_100 = 100;
+    private static final String MAURO_ROSSI = "MAURO ROSSI";
+    private static final String MARCO_VERDI = "MARCO VERDI";
+    private DeathNote deathNote;
+
+    @BeforeEach
+    void init(){
+        deathNote = new DeathNoteImplementation();
+    }
+
+    @Test
+    public void testIllegalRule(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            deathNote.getRule(0);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            deathNote.getRule(-1);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            deathNote.getRule(DeathNote.RULES.size() + 1);
+        });
+    }
+
+    @Test
+    public void testRules(){
+        for(String rule : DeathNote.RULES){
+            assertNotNull(rule);
+            assertFalse(rule.isBlank());
+        }
+    }
+
+    @Test
+    public void testWriteName(){
+        assertFalse(deathNote.isNameWritten(MARCO_VERDI));
+        deathNote.writeName(MARCO_VERDI);
+        assertTrue(deathNote.isNameWritten(MARCO_VERDI));
+        assertFalse(deathNote.isNameWritten(MAURO_ROSSI));
+        assertFalse(MARCO_VERDI.isBlank());
+    }
+
+    @Test
+    public void testWriteDeathCause() throws InterruptedException{
+        assertThrows(IllegalStateException.class, () -> {
+            deathNote.writeDeathCause("fall from stairs");
+        });
+        deathNote.writeName(MARCO_VERDI);
+        assertEquals("heart attack", deathNote.getDeathCause(MARCO_VERDI));
+        deathNote.writeName(MAURO_ROSSI);
+        assertTrue(deathNote.writeDeathCause("karting accident"));
+        assertEquals("karting accident", deathNote.getDeathCause(MAURO_ROSSI));
+        Thread.sleep(MS_TO_SLEEP_100);
+        assertFalse(deathNote.writeDeathCause("breaking neck"));
+        assertEquals("karting accident", deathNote.getDeathCause(MAURO_ROSSI));
+    }
+
+    @Test
+    public void testWriteDetails() throws InterruptedException{
+        assertThrows(IllegalStateException.class, () -> {
+            deathNote.writeDetails("heart attack");
+        });
+        deathNote.writeName(MARCO_VERDI);
+        assertEquals("", deathNote.getDeathDetails(MARCO_VERDI));
+        assertTrue(deathNote.writeDetails("Ran for too long"));
+        deathNote.writeName(MAURO_ROSSI);
+        Thread.sleep(MS_TO_SLEEP_6100);
+        assertFalse(deathNote.writeDetails("Got scared"));
+        assertEquals("", deathNote.getDeathDetails(MAURO_ROSSI));
+
+    }
 
 }
